35b05a2c909a8501a35bc5d5cbccb423cdda6305
AlinaZah31 <alina_shuvi@yahoo.com>
2024-07-05 14:10:01 +0200

    Update script.js (calling newGame() function only once)

--- script.js
+++ script.js
@@ -1,35 +1,32 @@
 livesBox.value = 7;
 const words = ["code", "break", "mountain", "vacation", "toddler"]
 const randomWord = words[Math.floor(Math.random() * words.length)];
+let lines = ""; 
 newGame();
 
 
 function newGame() {  
-    let lines = ""; 
     for (let i = 0; i < randomWord.length; ++i) {
         lines += "_" + " ";
     }
     document.getElementById("Searched word").innerHTML = lines;
-    return lines;
 }
 
 String.prototype.replaceAt = function(index, replacement) {
     return this.substring(0, index) + replacement + this.substring(index + replacement.length);
 };
 
-let modifiedLines = newGame();
-
 function checkLetter() {   
     for (let i = 0; i < randomWord.length; ++i) {
         if (letterBox.value == randomWord[i]) {
-            modifiedLines = modifiedLines.replaceAt(i * 2, randomWord[i]);
+            lines = lines.replaceAt(i * 2, randomWord[i]);
         }
     }
-    document.getElementById("Searched word").innerHTML = modifiedLines;
+    document.getElementById("Searched word").innerHTML = lines;
     if (!randomWord.includes(letterBox.value)) {
         --livesBox.value;
     }
-    displayMessage(livesBox.value, modifiedLines);
+    displayMessage(livesBox.value, lines);
 }
 
 function displayMessage(currentLives, currentLines) {
